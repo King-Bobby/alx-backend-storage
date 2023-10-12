2aa01e4c58f93592296cda620363653d5cbff5de
Noel Brown <noelbrowndboss@gmail.com>
2023-10-12 20:27:15 +0000

    changes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---Creates a table "users" if it doesn't exist
-CREATE TABLE if NOT EXISTS users (
+-- Creates a table 'users' if it doesn't exist
+CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	name VARCHAR(255)
