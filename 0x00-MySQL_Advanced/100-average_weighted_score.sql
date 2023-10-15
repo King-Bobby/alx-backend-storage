3d59a5fb249a3feadb8d7c7c94071d50d3a5f93e
Noel Brown <noelbrowndboss@gmail.com>
2023-10-15 21:13:36 +0000

    average

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,24 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE total_weighted_score FLOAT;
+    DECLARE total_weight INT;
+
+    SELECT SUM(c.score * p.weight) INTO total_weighted_score, total_weight
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
+
+    IF total_weight > 0 THEN
+        UPDATE users
+        SET average_score = total_weighted_score / total_weight
+        WHERE id = user_id;
+    ELSE
+        UPDATE users
+        SET average_score = 0
+        WHERE id = user_id;
+    END IF;
+END;
+//
+DELIMITER ;
