5da498b79199ee654c151ac2f9626ab644bb3fa6
Noel Brown <noelbrowndboss@gmail.com>
2023-10-15 21:17:09 +0000

    average

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,13 +3,13 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     DECLARE total_weighted_score FLOAT;
-    DECLARE total_weight INT;
-
-    SELECT SUM(c.score * p.weight) INTO total_weighted_score, total_weight
+    DECLARE total_weight FLOAT;
+    
+    SELECT SUM(c.score * p.weight), SUM(p.weight) INTO total_weighted_score, total_weight
     FROM corrections c
     JOIN projects p ON c.project_id = p.id
     WHERE c.user_id = user_id;
-
+    
     IF total_weight > 0 THEN
         UPDATE users
         SET average_score = total_weighted_score / total_weight
@@ -22,3 +22,4 @@ BEGIN
 END;
 //
 DELIMITER ;
+
